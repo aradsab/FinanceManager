0c044754bf704d7db1d409779e2ac33a2bfd3e79
Hussein <al.hussein2309@gmail.com>
2024-07-09 23:15:05 -0700

    update1

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ if [ "$OSTYPE" == "msys" ]; then
     #If it is not installed will get the latest version of it
         choco install git -y
     fi
-fi
-if [[ "$OSTYPE" == "darwin"* ]]; then
+elif [[ "$OSTYPE" == "darwin"* ]]; then
     if ! "$(which grep | grep -q "/opt/local/libexec/gnubin/")" ; then
         sudo port install grep
     fi
