2f7cda5525c26af92623f4d7b07ff3af935e6f4b
aradprsabet <80018126+aradsab@users.noreply.github.com>
2024-08-24 10:00:50 -0700

    changed directory

--- finance_manager_back/asgi.py
+++ finance_manager_back/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for FinanceManager_back project.
+ASGI config for finance_manager_back project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FinanceManager_back.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance_manager_back.settings')
 
 application = get_asgi_application()
