d771a8293411c6c5f95e85ee710e4322c79b1ea6
aradprsabet <80018126+aradsab@users.noreply.github.com>
2024-08-24 10:15:49 -0700

    changed directory

--- finance_manager_back/settings.py
+++ finance_manager_back/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'finance_manager_back.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'finance_manager_back.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
