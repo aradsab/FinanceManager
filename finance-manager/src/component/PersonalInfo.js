033ee41eba9b5b0c083c6b0ab44da8c3fba9a5c7
Hussein Al Aaref <62605937+husseinalaaref@users.noreply.github.com>
2024-12-26 21:23:04 -0800

    Personal Info

--- finance-manager/src/component/PersonalInfo.js
+++ finance-manager/src/component/PersonalInfo.js
@@ -1,22 +1,44 @@
 function PersonalInfo() {
     return (
-        <form className="max-w-md mx-auto p-4 bg-gray-100 rounded-md shadow">
+        <form className="p-4 bg-gray-100 rounded-md shadow">
             <h1 className="text-3xl font-semibold text-center">
                 Personal Information
             </h1>
-            <div className="flex mb-4 md:space-x-4">
-                <label className="block text-gray-700">First Name:</label>
-                <input
-                    type="text"
-                    className="border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200"
-                />
+            <div className="flex space-x-4 mb-4">
+                <div className="flex flex-col w-1/2">
+                    <label className="block text-gray-700">First Name:</label>
+                    <input
+                        type="text"
+                        className="border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200"
+                        placeholder="Enter first name"
+                    />
+                </div>
+                <div className="flex flex-col w-1/2">
+                    <label className="block text-gray-700">Last Name:</label>
+                    <input
+                        type="text"
+                        className="border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200"
+                        placeholder="Enter last name"
+                    />
+                </div>
             </div>
-            <div className="flex mb-4 md:space-x-4">
-                <label className="block text-gray-700">Last Name:</label>
-                <input
-                    type="text"
-                    className="border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200"
-                />
+            <div className="flex space-x-4 mb-4">
+                <div className="flex flex-col w-1/2">
+                    <label className="block text-gray-700">Email:</label>
+                    <input
+                        type="email"
+                        className="border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200"
+                        placeholder="Enter email"
+                    />
+                </div>
+                <div className="flex flex-col w-1/2">
+                    <label className="block text-gray-700">Phone Number:</label>
+                    <input
+                        type="tel"
+                        className="border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200"
+                        placeholder="Enter phone number"
+                    />
+                </div>
             </div>
         </form>
     );
