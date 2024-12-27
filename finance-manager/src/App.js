991c8db2dcdaf488212c0410e48a8b8ab28f9456
Hussein Al Aaref <62605937+husseinalaaref@users.noreply.github.com>
2024-12-26 20:42:58 -0800

    Personal Info

--- finance-manager/src/App.js
+++ finance-manager/src/App.js
@@ -1,11 +1,13 @@
 import NavBar from './component/NavBar';
 import './App.css';
 import TransactionForm from "./component/TransactionFrom";
+import PersonalInfo from "./component/PersonalInfo";
 
 function App() {
   return (
       <div>
           <NavBar />
+          <PersonalInfo />
           <TransactionForm />
       </div>
   );
---
+++ finance-manager/src/component/PersonalInfo.js
@@ -0,0 +1,22 @@
+function PersonalInfo() {
+    return (
+        <form className="max-w-md mx-auto p-4 bg-gray-100 rounded-md shadow">
+            <div className="flex mb-4 md:space-x-4">
+                <label className="block text-gray-700">First Name:</label>
+                <input
+                    type="text"
+                    className="border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200"
+                />
+            </div>
+            <div className="flex mb-4 md:space-x-4">
+                <label className="block text-gray-700">Last Name:</label>
+                <input
+                    type="text"
+                    className="border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-200"
+                />
+            </div>
+        </form>
+    );
+}
+
+export default PersonalInfo;
